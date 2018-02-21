1fefd9a563ddc41443cfd818d3a623f73f448319
Dominic Dorhat <aboldsduck@gmail.com>
2018-02-21 16:36:56 +0800

    Added comments

--- NottCS_Django/NottCS_Django/settings.py
+++ NottCS_Django/NottCS_Django/settings.py
@@ -37,7 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #External apps
     'rest_framework',
+
+    #Custom apps
     'jwt_auth',
 ]
 
--- NottCS_Django/jwt_auth/urls.py
+++ NottCS_Django/jwt_auth/urls.py
@@ -3,8 +3,13 @@
 from django.urls import path
 from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
 
+'''
+    all JWT token-related endpoints require POST method
+'''
+
 urlpatterns = [
     path('api-token-auth/', obtain_jwt_token),
     path('api-token-refresh/', refresh_jwt_token),
     path('api-token-verify/', verify_jwt_token)
+
 ]
