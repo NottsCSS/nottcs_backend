e1c83ed8f0f6bee4fc791a0ef280fb5121366f97
teykaiwen <tey.kaiwen@gmail.com>
2018-03-30 23:19:15 +0800

    commit

--- NottCS_Django/society/admin.py
+++ NottCS_Django/society/admin.py
@@ -1,8 +1,25 @@
 from django.contrib import admin
 from .models import *
-admin.site.register(Event)
+from django import forms
+from django.forms import widgets
+
 admin.site.register(EventTime)
 admin.site.register(Club)
 admin.site.register(Member)
 admin.site.register(Participant)
 admin.site.register(Attendance)
+
+class EventAdminForm( forms.ModelForm ):    
+    title = forms.CharField( max_length = 255, required = True )
+    
+
+class EventAdmin( admin.ModelAdmin ):
+    fields  = ['title']
+    form    = EventAdminForm
+    
+    def save_model(self, request, obj, form, change):
+        obj.title = "name here"
+        obj.save() 
+
+admin.site.register( Event, EventAdmin )
+
