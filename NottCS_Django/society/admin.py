b209a7e10ce9d89de861b09669dbe0d7ccd816d6
cwkang <cwk1998@hotmail.com>
2018-06-13 23:49:25 +0800

    Commented out test post save code in admin that was causing issue when attempting to save models

--- NottCS_Django/society/admin.py
+++ NottCS_Django/society/admin.py
@@ -6,14 +6,16 @@ admin.site.register(EventTime)
 admin.site.register(Club)
 admin.site.register(Member)
 admin.site.register(Participant)
-admin.site.register(Attendance)    
+admin.site.register(Attendance)
 
-class EventAdmin( admin.ModelAdmin ):
-    list_display=['title', 'description', 'created_timestamp', 'organizing_club', 'organizing_chairman', 'status', 'image', 'venue', 'additional_info']
 
-    def save_model(self, request, obj, form, change):
-        obj.title = "name here"
-        obj.save() 
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description', 'created_timestamp', 'organizing_club',
+                    'organizing_chairman', 'status', 'image', 'venue', 'additional_info']
 
-admin.site.register( Event, EventAdmin )
+    # def save_model(self, request, obj, form, change):
+    #     obj.title = "name here"
+    #     obj.save()
 
+
+admin.site.register(Event, EventAdmin)
